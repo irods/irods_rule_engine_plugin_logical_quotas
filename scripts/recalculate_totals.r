9fb7c133e2537a40863134cd31276b050c750b9c
Kory Draughn <korydraughn@renci.org>
2019-10-24 19:03:13 -0400

    Fixed some bugs and added some comments to README.

---
+++ scripts/recalculate_totals.r
@@ -0,0 +1,6 @@
+{
+    "operation": "logical_quotas_recalculate_totals",
+    "collection": "/tempZone/home/kory"
+}
+INPUT null
+OUTPUT null
---
+++ scripts/set_maximum_size_in_bytes.r
@@ -0,0 +1,7 @@
+{
+    "operation": "logical_quotas_set_maximum_size_in_bytes",
+    "collection": "/tempZone/home/kory",
+    "value": 3000
+}
+INPUT null
+OUTPUT null
