1d571387e9fdc2b38a4f9a5077c0ae6228864655
Kory Draughn <korydraughn@renci.org>
2024-04-21 09:45:25 -0400

    [#91] Run clang-format on all source files.

--- src/attributes.hpp
+++ src/attributes.hpp
@@ -7,34 +7,34 @@
 
 namespace irods
 {
-    class attributes final
-    {
-    public:
-        attributes(const std::string& _namespace,
-                   const std::string& _maximum_number_of_data_objects,
-                   const std::string& _maximum_size_in_bytes,
-                   const std::string& _total_number_of_data_objects,
-                   const std::string& _total_size_in_bytes)
-            : maximum_number_of_data_objects_{fmt::format("{}::{}", _namespace, _maximum_number_of_data_objects)}
-            , maximum_size_in_bytes_{fmt::format("{}::{}", _namespace, _maximum_size_in_bytes)}
-            , total_number_of_data_objects_{fmt::format("{}::{}", _namespace, _total_number_of_data_objects)}
-            , total_size_in_bytes_{fmt::format("{}::{}", _namespace, _total_size_in_bytes)}
-        {
-        }
+	class attributes final
+	{
+	  public:
+		attributes(const std::string& _namespace,
+		           const std::string& _maximum_number_of_data_objects,
+		           const std::string& _maximum_size_in_bytes,
+		           const std::string& _total_number_of_data_objects,
+		           const std::string& _total_size_in_bytes)
+			: maximum_number_of_data_objects_{fmt::format("{}::{}", _namespace, _maximum_number_of_data_objects)}
+			, maximum_size_in_bytes_{fmt::format("{}::{}", _namespace, _maximum_size_in_bytes)}
+			, total_number_of_data_objects_{fmt::format("{}::{}", _namespace, _total_number_of_data_objects)}
+			, total_size_in_bytes_{fmt::format("{}::{}", _namespace, _total_size_in_bytes)}
+		{
+		}
 
-        // clang-format off
-        const std::string& maximum_number_of_data_objects() const { return maximum_number_of_data_objects_; }
-        const std::string& maximum_size_in_bytes() const          { return maximum_size_in_bytes_; }
-        const std::string& total_number_of_data_objects() const   { return total_number_of_data_objects_; }
-        const std::string& total_size_in_bytes() const            { return total_size_in_bytes_; }
-        // clang-format on
+		// clang-format off
+		const std::string& maximum_number_of_data_objects() const { return maximum_number_of_data_objects_; }
+		const std::string& maximum_size_in_bytes() const          { return maximum_size_in_bytes_; }
+		const std::string& total_number_of_data_objects() const   { return total_number_of_data_objects_; }
+		const std::string& total_size_in_bytes() const            { return total_size_in_bytes_; }
+		// clang-format on
 
-    private:
-        std::string maximum_number_of_data_objects_;
-        std::string maximum_size_in_bytes_;
-        std::string total_number_of_data_objects_;
-        std::string total_size_in_bytes_;
-    }; // class attributes
+	  private:
+		std::string maximum_number_of_data_objects_;
+		std::string maximum_size_in_bytes_;
+		std::string total_number_of_data_objects_;
+		std::string total_size_in_bytes_;
+	}; // class attributes
 } // namespace irods
 
 #endif // IRODS_LOGICAL_QUOTAS_ATTRIBUTES_HPP
--- src/logical_quotas_error.hpp
+++ src/logical_quotas_error.hpp
@@ -5,26 +5,25 @@
 
 namespace irods
 {
-    class logical_quotas_error
-        : public std::runtime_error
-    {
-    public:
-        using error_code_type = int;
+	class logical_quotas_error : public std::runtime_error
+	{
+	  public:
+		using error_code_type = int;
 
-        logical_quotas_error(const char* _msg, error_code_type _error_code) noexcept
-            : std::runtime_error{_msg}
-            , error_code_{_error_code}
-        {
-        }
+		logical_quotas_error(const char* _msg, error_code_type _error_code) noexcept
+			: std::runtime_error{_msg}
+			, error_code_{_error_code}
+		{
+		}
 
-        auto error_code() const noexcept -> error_code_type
-        {
-            return error_code_;
-        }
+		auto error_code() const noexcept -> error_code_type
+		{
+			return error_code_;
+		}
 
-    private:
-        error_code_type error_code_;
-    };
+	  private:
+		error_code_type error_code_;
+	};
 } // namespace irods
 
 #endif // IRODS_LOGICAL_QUOTAS_LOGICAL_QUOTAS_ERROR_HPP
